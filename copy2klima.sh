d7cd39439b016cb652e8f86011e76892faab12b8
rhinophile <bijan.text@gmail.com>
2017-06-16 17:03:45 -0400

    .

--- copy2klima.sh
+++
@@ -1,1 +0,0 @@
-scp ~/Projects/drawROI/init.R ~/Projects/drawROI/ui.R ~/Projects/drawROI/server.R ~/Projects/drawROI/funcs.R ~/Projects/drawROI/copy2shiny.sh bijan@klima.sr.unh.edu:/home/bijan/rscripts/drawROI
