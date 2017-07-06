f13e0c8039e09b1698296cd26dc1bdc93a01f3c4
rhinophile <bijan.text@gmail.com>
2017-07-06 09:50:19 -0400

    test

--- server.R
+++ server.R
@@ -202,7 +202,7 @@ shinyServer(function(input, output, session) {
   roipath <- reactive({
     tmp <- (paste0('/data/archive/', input$siteName,'/ROI/'))
     tmp <- ('/home/shiny/drawROI/ROI/')
-    tmp <- tempdir()
+    # tmp <- tempdir()
     
     if(getwd()==bijanWD) 
       tmp <- (paste0('phenocamdata/data/archive/', input$siteName,'/ROI/'))
