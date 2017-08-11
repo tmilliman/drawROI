4748e9f387e03acd30894e0322ba57267caec124
rhinophile <bijan.text@gmail.com>
2017-08-11 14:38:17 -0400

    testmode

--- global.R
+++ global.R
@@ -1,6 +1,9 @@
-TEST_MODE <- TRUE
 
 midddayListPath <- '/home/shiny/middayList/'
+
+TEST_MODE <- FALSE
+if(getwd()=='/Users/bijan/Projects/drawROI')TEST_MODE <- TRUE
+
 
 if(TEST_MODE) midddayListPath <- 'midddayListPath/'
 
